26c4c9a15d13439706e155b3f382ec9f6895654e
Gautam Jha <gauti.jha37@gmail.com>
2024-02-20 19:24:44 +0530

    add .clang-format

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,46 +1,46 @@
 #include "logger.h"
 
-namespace TIME{
-    void get_time(char **buf)
-    {
-        auto currentTime = std::chrono::high_resolution_clock::now();
-        auto nanoSeconds =
-            std::chrono::time_point_cast<std::chrono::nanoseconds>(currentTime);
+namespace TIME
+{
+void get_time(char** buf)
+{
+	auto currentTime = std::chrono::high_resolution_clock::now();
+	auto nanoSeconds = std::chrono::time_point_cast<std::chrono::nanoseconds>(currentTime);
 
-        auto nanoSecondsCount = nanoSeconds.time_since_epoch().count();
+	auto nanoSecondsCount = nanoSeconds.time_since_epoch().count();
 
-        // Convert nanoseconds to seconds and fractional seconds
-        auto fracSeconds = nanoSecondsCount % 1'000'000'000;
+	// Convert nanoseconds to seconds and fractional seconds
+	auto fracSeconds = nanoSecondsCount % 1'000'000'000;
 
-        // Convert seconds to std::time_t
-        std::time_t time =
-            std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	// Convert seconds to std::time_t
+	std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
-        // Convert time to struct tm in local time zone
-        std::tm *tm = std::localtime(&time);
+	// Convert time to struct tm in local time zone
+	std::tm* tm = std::localtime(&time);
 
-        std::strftime(*buf, 9, "%H:%M:%S.", std::localtime(&time));
-        std::snprintf(*buf + 9, 10, "%09lld", fracSeconds);
-    }
+	std::strftime(*buf, 9, "%H:%M:%S.", std::localtime(&time));
+	std::snprintf(*buf + 9, 10, "%09lld", fracSeconds);
 }
+} // namespace TIME
 
 my_logger::my_logger()
 {
-    m_buf = new char[TIME_BUF_SIZE];
+	m_buf = new char[TIME_BUF_SIZE];
 }
 
-template <typename... Args> 
-void my_logger::info(const char* format, Args... args) {
-    // TIME::get_time();
-    // m_to_print.emplace_back("[%s] [%s] " format "\n", m_buf, "INFO", _args...); 
-//   std::lock_guard<std::mutex> lg(mu_lines);
-//   m_lines.emplace_back(std::forward<string_type>(input),
-                    //    my_logger::log_type::INFO);
+template <typename... Args>
+void my_logger::info(const char* format, Args... args)
+{
+	// TIME::get_time();
+	// m_to_print.emplace_back("[%s] [%s] " format "\n", m_buf, "INFO", _args...);
+	//   std::lock_guard<std::mutex> lg(mu_lines);
+	//   m_lines.emplace_back(std::forward<string_type>(input),
+	//    my_logger::log_type::INFO);
 }
 
 template <typename string_type>
-void my_logger::warn(string_type input) {
-  std::lock_guard<std::mutex> lg(mu_lines);
-  m_lines.push_back(std::make_pair(std::forward<string_type>(input),
-                                   my_logger::log_type::WARN));
+void my_logger::warn(string_type input)
+{
+	std::lock_guard<std::mutex> lg(mu_lines);
+	m_lines.push_back(std::make_pair(std::forward<string_type>(input), my_logger::log_type::WARN));
 }
--- src/logger.h
+++ src/logger.h
@@ -15,48 +15,48 @@
 
 namespace TIME
 {
-    void get_time();
+void get_time();
 } // namespace TIME
 
 class my_logger
 {
-    enum class log_type : uint8_t
-    {
-        INFO,
-        ERROR,
-        WARN,
-        ABORT
-    };
+	enum class log_type : uint8_t
+	{
+		INFO,
+		ERROR,
+		WARN,
+		ABORT
+	};
 
-    static constexpr size_t TIME_BUF_SIZE = 19;
-    // static constexpr LOGGING_DELAY
-    std::deque<std::pair<std::string, log_type>> m_lines;
-    std::mutex mu_lines;
-    char *m_buf;
-    std::vector<const char*> m_to_print;
+	static constexpr size_t TIME_BUF_SIZE = 19;
+	// static constexpr LOGGING_DELAY
+	std::deque<std::pair<std::string, log_type>> m_lines;
+	std::mutex mu_lines;
+	char* m_buf;
+	std::vector<const char*> m_to_print;
 
-    #define LOG(_type, _fmt, _args...) \
-    TIME::get_time(); \
-    // m_to_print.emplace_back("[%s] [%s] " _fmt "\n", m_buf, _type, ##_args); 
+#define LOG(_type, _fmt, _args...)                                                                 \
+	TIME::get_time();                                                                              \
+	// m_to_print.emplace_back("[%s] [%s] " _fmt "\n", m_buf, _type, ##_args);
 
-    #define LOG_INFO(_fmt, _args...) LOG("INFO", _fmt, ##_args)
-    #define LOG_WARN(_fmt, _args...) LOG("WARN", _fmt, ##_args)
-    #define LOG_ERROR(_fmt, _args...) LOG("ERROR", _fmt, ##_args)
-    #define LOG_ABORT(_fmt, _args...) LOG("ABORT", _fmt, ##_args)
+#define LOG_INFO(_fmt, _args...) LOG("INFO", _fmt, ##_args)
+#define LOG_WARN(_fmt, _args...) LOG("WARN", _fmt, ##_args)
+#define LOG_ERROR(_fmt, _args...) LOG("ERROR", _fmt, ##_args)
+#define LOG_ABORT(_fmt, _args...) LOG("ABORT", _fmt, ##_args)
 
 public:
-    my_logger();
-    template <typename... Args> 
-    void info(const char* format, Args... args);
+	my_logger();
+	template <typename... Args>
+	void info(const char* format, Args... args);
 
-    template <typename string_type>
-    void error(string_type input);
+	template <typename string_type>
+	void error(string_type input);
 
-    template <typename string_type>
-    void warn(string_type input);
+	template <typename string_type>
+	void warn(string_type input);
 
-    template <typename string_type>
-    void abort(string_type input);
+	template <typename string_type>
+	void abort(string_type input);
 
-    void print();
+	void print();
 };
